3e0da7ed329dcf0e5d5969ee8797fe322b657813
Ravikiran Radhakrishnan <rkiran0307@gmail.com>
2022-05-05 11:40:43 -0500

    test

---
+++ Index.ts
@@ -0,0 +1,10 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResultV2, Handler } from 'aws-lambda' ;
+
+export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> => {
+const response = {
+    statusCode: 200,
+    body: JSON.stringify('Hello from Lambda! 12344'),
+};
+return response;
+};
+    
